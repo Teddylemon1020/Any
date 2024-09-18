461f70a54dfe28cf60337faab166ab7939890e59
SOC Analyst trainee and Python programmer <103621185+Teddylemon1020@users.noreply.github.com>
2024-09-18 11:16:56 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,15 @@
+let gameStart = False;
+let winningChar = None; 
+let rouletteRange = None; 
+let Ingame = False;
+
+let players = [{ playerID: null, CurrentPoints: null, bettedAmount: null},
+    { playerID: null, CurrentPoints: null, bettedAmount: null},
+    { playerID: null, CurrentPoints: null, bettedAmount: null},
+    { playerID: null, CurrentPoints: null, bettedAmount: null},
+    { playerID: null, CurrentPoints: null, bettedAmount: null}
+];
+
+function Bangkero(newBankero, oldBankero){
+    
+}

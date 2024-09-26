10883f16a85e53cfd16447d7f0551aa900d50bb4
SOC Analyst trainee and Python programmer <103621185+Teddylemon1020@users.noreply.github.com>
2024-09-26 08:57:03 +0800

    Add files via upload

--- roulette/functions.js
+++ roulette/functions.js
@@ -1,7 +1,11 @@
-let gameStart = False;
+let gameStart = false;
 let winningChar = None; 
 let rouletteRange = None; 
-let Ingame = False;
+let Ingame = false;
+let Number_color_btn_active = true;
+let Numbers_btn = true; 
+let commands = true;
+
 
 let players = [{ playerID: null, CurrentPoints: null, bettedAmount: null},
     { playerID: null, CurrentPoints: null, bettedAmount: null},
@@ -10,6 +14,97 @@ let players = [{ playerID: null, CurrentPoints: null, bettedAmount: null},
     { playerID: null, CurrentPoints: null, bettedAmount: null}
 ];
 
-function Bangkero(newBankero, oldBankero){
+
+function Bangkero(newBankero, oldBankero) {
+    let newBankeroIndex = players.indexOf(newBankero);
+    let oldBankeroIndex = players.indexOf(oldBankero);
     
+    players.splice(newBankeroIndex, 1);  
+    players.splice(oldBankeroIndex, 1);  
+    players.splice(oldBankeroIndex, 0, newBankero);
+    players.splice(newBankeroIndex, 0, oldBankero);
 }
+
+function freeze_buttons() {
+    if (Ingame === true && spinning === true) {  
+        Spin_btn = false;
+        commands = false;
+        Number_color_btn_active = false;
+        Numbers_btn = false; 
+
+        freeze_colors();
+        freeze_commands();
+        freeze_Numbers();
+
+    }
+}
+
+function freeze_colors(){
+    let buttons = document.querySelectorAll('.color-btn.green, .color-btn.black, .color-btn.red');
+    if (Number_color_btn_active == false){
+        buttons.forEach(button =>{
+            button.classList.add('inactive');
+        });
+    } else {
+        buttons.forEach(button =>{
+            button.classList.remove('inactive');
+        });
+    }
+}
+
+function freeze_commands(){
+    let buttons = document.querySelectorAll('.action-btn');
+    if (commands == false){
+        buttons.forEach(button =>{
+            button.classList.add('inactive');
+        });
+    } else {
+        buttons.forEach(button =>{
+            button.classList.remove('inactive');
+        });
+    }
+}
+
+function freeze_Numbers(){
+    let buttons = document.querySelectorAll('.push-btn');
+    if (Numbers_btn == false){
+        buttons.forEach(button =>{
+            button.classList.add('inactive');
+        });
+    } else {
+        buttons.forEach(button =>{
+            button.classList.remove('inactive');
+        });
+    }
+}
+
+function Playing(){
+    startFunc();
+    if (Ingame === true){
+        
+
+    }
+
+}
+
+function chip_distribution(selectedPlayer, added_chips){
+    
+    let summation = players[selectedPlayer].CurrentPoints += added_chips;
+
+    
+
+}
+
+//add 10 seconds delay 
+//add console
+// add freeze to spin button 
+//remove undo and redo button
+// ui na ginawa ni sir ryko 
+// add remove player
+// add alert for the number that is selected in the roulette
+// add icon button to change the profile of the user
+// add unique id
+//read and write new value of chips from current game
+//terms and agreement and rules of the game
+
+DelayNode(10000) //10 seconds
